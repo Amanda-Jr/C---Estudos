6c059267bd73bed51f384d5e67adfa734c83bb4c
Amanda-Jr <amandadasilvapereira2004@gmail.com>
2023-08-22 11:52:17 -0300

    Ex4

--- LPI/aula1708Ex4-Vetor.cpp
+++ LPI/aula1708Ex4-Vetor.cpp
@@ -4,3 +4,34 @@ using namespace std;
 /*
 Leia um vetor e verifique se há elementos repetidos
 */
+
+
+void ler_vetor(int vetor[], int tam){
+    for(int i = 0; i<tam; i++){
+        cout << "Posição " << i+1 << ": ";
+        cin >> vetor[i];
+    }
+}
+
+string repetidos(int vet[], int tam){
+    for(int i=0; i<tam; i++){
+        for(int j=i+1; j<tam; j++){
+            if (vet[i] == vet[j]){
+                return "Há elementos repetidos";
+            }
+        }
+    }
+    return "Não há elementos repetidos";
+}
+
+int main(){
+    const int tam = 7;
+    int vet[tam];
+
+    
+    ler_vetor(vet, tam);
+    cout<< repetidos(vet, tam)<<endl;
+
+
+    return 0;
+}
