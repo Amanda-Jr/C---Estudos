d18d885e33203cfa95fae9f534caf43285be7d48
Amanda-Jr <amandadasilvapereira2004@gmail.com>
2023-08-26 23:29:29 -0300

    lab lpi q3

--- Lab LPI/aula02Ex3.cpp
+++ Lab LPI/aula02Ex3.cpp
@@ -3,38 +3,47 @@ using namespace std;
 
 //3- Construa um programa que leia uma palavra e a reescreva de trás para frente
 
-//INACABADA
-
-/*
-void ler_vetor(char vet[],int tam){
+void ler_vetor(char vet[], char l){
     cout << "Escreva uma palavra de 5 letras."<<endl;
-    for(int i = 0; i<tam; i++){
-        cout << "Digite a " << i+1 << "° letra: ";
-        cin >> vet[i];
+    int i = 0;
+    while((l = getchar())!='\n'){  
+        vet[i] = l;
+        i++;
     }
 }
 
-void inverter(char vet[], int tam){
-    char invertida[tam];
+void inverter(char vet[], char inv[], int tam){    
     int j=0;
 
     for(int i=tam; i>=0; i--){
-        invertida[j] = vet[i];
+        inv[j] = vet[i];
         j++;
     }
 
-    cout << invertida << endl;
+}
+
+void imprimir(char vetor[], int tam){
+    for(int i=0; i<=tam; i++){
+        cout << vetor[i];
+    }
+    cout << endl;
 }
 
 int main(){
-
     int tam = 5;
     char palavra[tam];
+    char invertida[tam];
+    char l;
 
-    ler_vetor(palavra, tam);
-    inverter(palavra, tam);
+    ler_vetor(palavra, l);
+    inverter(palavra, invertida, tam);
+
+    cout << "Palavra original: " << endl;
+    imprimir(palavra, tam);
+    cout << "Palavra invetida: " << endl;
+    imprimir(invertida, tam);
+
 
 
     return 0;
 }
-*/
